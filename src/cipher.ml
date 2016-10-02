45dac392a1a24d0dc607076b4e7f2b19dcea1daa
Dan McHenry <danpmch@gmail.com>
2016-10-01 22:08:51 -0400

    Allow rotating an input argument an arbitrary amount

--- src/cipher.ml
+++ src/cipher.ml
@@ -1,10 +1,10 @@
 
 open Batteries
 
-let rot13char c =
+let rotNChar (n : int) (c : char) : char =
    let unascii i = i - (int_of_char 'a') in
    let ascii i = i + (int_of_char 'a') in
-   let rot i = (i + 13) mod 26 in
+   let rot i = (i + n) mod 26 in
    Char.lowercase c
  |> int_of_char
  |> unascii
@@ -12,15 +12,24 @@ let rot13char c =
  |> ascii
  |> char_of_int
 
+let rotNString (n : int) : string -> string = String.map (rotNChar n)
+
+let rot13char = rotNChar 13
 let rot13 text = String.map rot13char text
 
+let rotAll text =
+   let shifts = List.of_enum (1 --^ 26) in
+   let shiftFuncs = List.map rotNString shifts in
+   List.map ((|>) text) shiftFuncs
+
 let () =
-   let cipher = rot13 "test" in
-   let uncipher = rot13 cipher in
+   let text = Sys.argv.(1) in
+   let decrypts = rotAll text in
    (
-   print_string cipher;
+   print_string "Applying all decryptions to: ";
+   print_string text;
    print_newline ();
-   print_string uncipher;
+   print_string (String.join "\n" decrypts);
    print_newline ();
    )
 
